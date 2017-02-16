fb57d4de22aaacff24bd57297bac900233f34fa3
Emily Sershon and Isaiah Wales <isaiah@>
2017-02-15 19:48:01 -0800

    add js that separates our front end and back end logic, changed results of calculated values to show up in html rather than in alerts

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,42 @@
-// var add = function(number1, number2) {
-//   return number1 + number2;
-// };
-//
-// var number1 = parseInt(prompt("Enter a number:"));
-// var number2 = parseInt(prompt("Enter another number:"));
-// var result = add(number1, number2);
-// alert(result);
-//
-// var subtract = function(number1, number2) {
-//   return number1 - number2;
-// };
-//
-// var number1 = parseInt(prompt("Enter a number:"));
-// var number2 = parseInt(prompt("Enter another number:"));
-// var result = subtract(number1, number2);
-// alert(result);
-//
+//Backend logic:
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+
+//Front-end logic:
+$(document).ready(function() {
+
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+    //
+    // var number1 = parseInt($("#add1").val());
+    // var number2 = parseInt($("#add2").val());
+    // var result = subtract(number1, number2);
+    // alert(result);
+  });
+});
+
+
+
+
+
+
+
+
+
 // var multiply= function(number1, number2) {
 //   return number1 * number2;
 // };
-//
+
 // var number1 = parseInt(prompt("Enter a number:"));
 // var number2 = parseInt(prompt("Enter another number:"));
 // var result = multiply(number1, number2);
