0ff2d6c9eb4d28a228b4d4a5aebedb11df70f38d
Emily Sershon and Isaiah Wales <emily@>
2017-02-15 20:15:32 -0800

    added subtraction, multiplication, and division capabilities

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,14 @@ var add = function(number1, number2) {
   return number1 + number2;
 };
 
+var multiply= function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
 
 //Front-end logic:
 $(document).ready(function() {
@@ -16,7 +24,28 @@ $(document).ready(function() {
     var number1 = parseInt($("#add1").val());
     var number2 = parseInt($("#add2").val());
     var result = add(number1, number2);
-    $("#output").text(result);
+    $("#addoutput").text(result);
+  });
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#subtract1").val());
+    var number2 = parseInt($("#subtract2").val());
+    var result = subtract(number1, number2);
+    $("#subtractoutput").text(result);
+  });
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#multiply1").val());
+    var number2 = parseInt($("#multiply2").val());
+    var result = multiply(number1, number2);
+    $("#multiplyoutput").text(result);
+  });
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#divide1").val());
+    var number2 = parseInt($("#divide2").val());
+    var result = divide(number1, number2);
+    $("#divideoutput").text(result);
     //
     // var number1 = parseInt($("#add1").val());
     // var number2 = parseInt($("#add2").val());
@@ -33,18 +62,13 @@ $(document).ready(function() {
 
 
 
-// var multiply= function(number1, number2) {
-//   return number1 * number2;
-// };
+
 
 // var number1 = parseInt(prompt("Enter a number:"));
 // var number2 = parseInt(prompt("Enter another number:"));
 // var result = multiply(number1, number2);
 // alert(result);
 //
-// var divide = function(number1, number2) {
-//   return number1 / number2;
-// };
 //
 // var number1 = parseInt(prompt("Enter a number:"));
 // var number2 = parseInt(prompt("Enter another number:"));
